d72e12d484be45f2d528c0da022e9d2f10d5b9fb
MBguimaraes <104425878+MateusBorgesGuimaraes@users.noreply.github.com>
2024-07-07 19:19:00 -0300

    Começo da responsividade

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,35 @@
+'use client';
+
 import LogoIcon from '@/icons/logo';
 import styles from './header.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
+import React from 'react';
 
 export default function Header() {
+  const [active, setActive] = React.useState(false);
+
+  const handleActive = () => {
+    setActive(!active);
+  };
+
   return (
     <header className={`${styles.headerContainer} container`}>
       <section className={`${styles.header}`}>
-        <div>
+        <div className={styles.iconHeader}>
           <LogoIcon />
         </div>
 
-        <nav>
+        <button onClick={handleActive} className={styles.buttonMenu}>
+          <Image
+            src={'/assets/icons/mobilemenu-40-40.svg'}
+            alt="mobile menu"
+            width={40}
+            height={40}
+          />
+        </button>
+
+        <nav className={`${styles.navegacao} ${active ? styles.active : ''}`}>
           <ul>
             <li>
               <Link href={`#sobre`}>sobre</Link>{' '}
